a0cefd23a0c9fd2bd63faa191700da5a756f02df
richard fontaine <ririfonfon10@gmail.com>
2021-07-04 13:11:24 +0200

    Create variable.h

---
+++ micro_pro_slave_capa/src/variable.h
@@ -0,0 +1,31 @@
+#ifndef variable_h
+#define variable_h
+
+/***************** variable potentiomètre **************/
+int valaniv_h = 0; //variable de niveau haut de a
+int valaniv_b = 0; //variable de niveau bas de a
+int valdniv_h = 0; //variable de niveau haut de d
+int valdniv_b = 0; //variable de niveau bas de d
+
+/************************* Variable ********************/
+int z;                 //
+const int taille = 20; //
+long d_t;              //
+long d;                // touch 9
+long d_d[taille];      //
+int e;                 // map touch 9
+int f;                 // map de e
+
+long a_t;         //
+long a;           // touch 6
+long a_a[taille]; //
+int b;            // map touch 6
+int c;            // map de b
+
+int aniv_h; //
+int aniv_b; //
+
+int dniv_h; //
+int dniv_b; //
+
+#endif
